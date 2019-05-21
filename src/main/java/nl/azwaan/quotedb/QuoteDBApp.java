108188023aa4c7d4e6c3ad39aff8e2b0fe040a30
ASZwaan <aronzwaan@solcon.nl>
2019-05-21 13:04:49 +0200

    Remove commented code

--- src/main/java/nl/azwaan/quotedb/QuoteDBApp.java
+++ src/main/java/nl/azwaan/quotedb/QuoteDBApp.java
@@ -22,37 +22,6 @@ public class QuoteDBApp extends Jooby {
                 .schema(TableCreationMode.CREATE_NOT_EXISTS));
 
         use(new RestAPIModule());
-
-        /* onStart(() -> {
-            // Run liquibase update script
-            DataSource dataSource = require(DataSource.class);
-            Connection conn = dataSource.getConnection();
-            Liquibase lb = new Liquibase("conf/liquibase.xml",
-                    new FileSystemResourceAccessor(Paths.get("").toAbsolutePath().toString()),
-                    new JdbcConnection(conn));
-            lb.update(new Contexts());
-
-        }); */
-
-        /* path("/api", () -> {
-            get("/categories", () -> {
-                EntityStore store = require(EntityStore.class);
-                Object res = store.select(Category.class).get();
-                return res;
-            });
-
-            get("/quotes", () -> {
-                EntityStore store = require(EntityStore.class);
-                Object res =  store.select(Quote.class).get();
-                return res;
-            });
-
-            get("/labels", () -> {
-                EntityStore store = require(EntityStore.class);
-                Object res =  store.select(Label.class).get();
-                return res;
-            });
-        }); */
     }
 
     public static void main(final String[] args) {

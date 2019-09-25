2bb2ea24788e3229584737afb93ed9294cdd8abd
ASZwaan <aronzwaan@solcon.nl>
2019-09-25 11:07:54 +0200

    Add register user test + use OpenJDK8 on Travis

--- src/test/java/nl/azwaan/quotedb/integration/api/AuthAPITests.java
+++ src/test/java/nl/azwaan/quotedb/integration/api/AuthAPITests.java
@@ -1,4 +1,36 @@
 package nl.azwaan.quotedb.integration.api;
 
+import io.requery.EntityStore;
+import io.requery.query.Result;
+import nl.azwaan.quotedb.models.User;
+import org.junit.Test;
+import org.mindrot.jbcrypt.BCrypt;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+
 public class AuthAPITests extends APITest {
+
+    @Test
+    public void testRegister() {
+        postBase()
+                .body("{\"userName\": \"user1\", \"password\": \"pwd1\"}")
+                .post("/auth/register")
+                .then()
+                .assertThat()
+                .statusCode(201)
+                .body("userName", equalTo("user1")).and()
+                .body("password", equalTo(null)); // Hashed pwd should not be published
+
+        final EntityStore store = app.require(EntityStore.class);
+
+        final User user = ((Result<User>) store.select(User.class)
+                .get())
+                .first();
+
+        assertThat(user.getUserName(), equalTo("user1"));
+        assertThat(BCrypt.checkpw("pwd1", user.getPassword()), is(true));
+
+    }
+
 }

c7cf72921a0e65d2c6a64c81c8645ef2bd986720
ASZwaan <aronzwaan@solcon.nl>
2019-10-17 16:38:22 +0200

    Fix migration scripts

--- src/main/resources/db/migration/v1.0.2__Change_bio_definition_to_mediumtext.sql
+++ src/main/resources/db/migration/v1.0.2__Change_bio_definition_to_mediumtext.sql
@@ -1,12 +1,15 @@
 delimiter ;;
 
-drop procedure if exists biography_medium_text();;
+drop procedure if exists biography_medium_text;;
+
 create procedure biography_medium_text ()
 begin
     -- When table not exists there is no problem, requery will create it correct
-    declare continue handler for 42102 begin end
-    ALTER TABLE author CHANGE biography biography MEDIUMTEXT
+    declare continue handler for 42102 begin end;
+    declare continue handler for 1146 begin end;
+    declare continue handler for 1060 begin end;
+    ALTER TABLE author CHANGE biography biography MEDIUMTEXT;
 end;;
 
 call biography_medium_text();;
-drop procedure if exists biography_medium_text();;
+drop procedure if exists biography_medium_text;;

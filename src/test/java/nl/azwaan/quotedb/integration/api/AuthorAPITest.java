70b4abdd9742d53661d7b59b018f18f773ddcb81
ASZwaan <aronzwaan@solcon.nl>
2019-10-17 15:50:11 +0200

    Add author delete test

--- src/test/java/nl/azwaan/quotedb/integration/api/AuthorAPITest.java
+++ src/test/java/nl/azwaan/quotedb/integration/api/AuthorAPITest.java
@@ -1,6 +1,9 @@
 package nl.azwaan.quotedb.integration.api;
 
 import io.requery.EntityStore;
+import io.requery.query.Scalar;
+import io.requery.query.Selection;
+import io.requery.query.WhereAndOr;
 import nl.azwaan.quotedb.models.Author;
 import nl.azwaan.quotedb.models.User;
 import org.junit.Test;
@@ -8,6 +11,7 @@ import org.junit.Test;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.nullValue;
 
@@ -46,7 +50,7 @@ public class AuthorAPITest extends AuthenticatedTest {
                 .statusCode(200);
 
         // Check patch applied and all properties the same
-        getBase()
+        postBase()
                 .get("/api/authors")
                 .then()
                 .statusCode(200)
@@ -59,5 +63,31 @@ public class AuthorAPITest extends AuthenticatedTest {
                 .body("data[0].initials", equalTo("C.J.H."))
                 .body("data[0].dateOfBirth", equalTo("1812-02-07"))
                 .body("data[0].user", nullValue());
+    }
+
+    @Test
+    public void testDeleteAuthor() throws ParseException {
+        User user = getUser();
+
+        Author author = new Author();
+        author.setUser(user);
+        author.setFirstName("Charles");
+        author.setMiddleName("");
+        author.setLastName("Dickens");
+        author.setInitials("C.J.H.");
+        author.setDateOfBirth(new SimpleDateFormat("dd-MM-yyyy").parse("07-02-1812"));
+
+        EntityStore store = app.require(EntityStore.class);
+        store.insert(author);
+        store.refresh(author);
+
+        postBase()
+                .delete("/api/authors/{id}", author.getId())
+                .then()
+                .statusCode(200);
+
+        assertThat(((WhereAndOr<Scalar<Integer>>)store.count(Author.class)
+                .where(Author.DELETED.eq(false)))
+                .get().value(), equalTo(0));
     }
 }

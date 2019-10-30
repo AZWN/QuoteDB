61c8d8ce4fec5fc9b00031fc13c7b2af3292fb29
ASZwaan <aronzwaan@solcon.nl>
2019-10-30 15:55:51 +0100

    Move paging to filterbuilder

--- src/main/java/nl/azwaan/quotedb/api/filters/FilterBuilder.java
+++ src/main/java/nl/azwaan/quotedb/api/filters/FilterBuilder.java
@@ -2,22 +2,36 @@ package nl.azwaan.quotedb.api.filters;
 
 import io.requery.query.Return;
 import io.requery.query.Selection;
-import io.requery.query.WhereAndOr;
 
 public interface FilterBuilder {
     /**
      * Function building the filter over the query.
      * @param <T> the result Type of the query
      * @param baseQuery The unfiltered query.
+     * @param includePaging If true, the filter builder should add offset and limit constraints.
      * @return The filtered query.
      */
-    <T> WhereAndOr<T> addFilter(Selection<T> baseQuery);
+    <T> Return<T> buildQuery(Selection<T> baseQuery, boolean includePaging);
 
     /**
-     * Function finalizing the query (adding paging and limits etc).
-     * @param <T> The return type fo the query
-     * @param filteredQuery The query with filters
-     * @return The finalized query
+     * Default function building the filter over the query. Will include paging.
+     * @param <T> the result Type of the query
+     * @param baseQuery The unfiltered query.
+     * @return The filtered query with paging.
      */
-    <T> Return<T> finalizeQuery(WhereAndOr<T> filteredQuery);
+    default <T> Return<T> buildQuery(Selection<T> baseQuery) {
+        return buildQuery(baseQuery, true);
+    }
+
+    /**
+     * Returns the size of the result set of this query. Returns (-1) if no page size.
+     * @return The result set size
+     */
+    int getResultCount();
+
+    /**
+     * Returns the offset of the query (the skipped results). Defaults to 0.
+     * @return The query offset
+     */
+    int getOffset();
 }
